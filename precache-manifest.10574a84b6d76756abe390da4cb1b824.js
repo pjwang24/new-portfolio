df783119d2ec7df92ba7b4c47bbce981f13bd648
Peter Wang <pjwang2324@berkeley.edu>
2024-06-15 15:17:37 -0700

    Updates

---
+++ precache-manifest.10574a84b6d76756abe390da4cb1b824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96d09dcd7f5d33488aec595b211a6be",
+    "url": "/pjwang24/new-portfolio.git/index.html"
+  },
+  {
+    "revision": "b326b5db4087d0734098",
+    "url": "/pjwang24/new-portfolio.git/static/css/main.cf070577.chunk.css"
+  },
+  {
+    "revision": "e6ec829cd51cae75a38e",
+    "url": "/pjwang24/new-portfolio.git/static/js/2.52752374.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/pjwang24/new-portfolio.git/static/js/2.52752374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d85478fca66896e8fb",
+    "url": "/pjwang24/new-portfolio.git/static/js/3.c08283b3.chunk.js"
+  },
+  {
+    "revision": "b326b5db4087d0734098",
+    "url": "/pjwang24/new-portfolio.git/static/js/main.5464ef10.chunk.js"
+  },
+  {
+    "revision": "9c1ec8a6d97c749bf116",
+    "url": "/pjwang24/new-portfolio.git/static/js/runtime-main.26322804.js"
+  }
+]);
